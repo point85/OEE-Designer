927b2467f209e0f151c389d34fd1520fc3c038df
billran <kentra@pacbell.net>
2018-03-09 17:43:10 -0800

    wip

--- run-collector-app.bat
+++ run-collector-app.bat
@@ -1,1 +1,1 @@
-start javaw.exe -jar oee-apps-1.0.0-jfx.jar COLLECTOR jdbc:sqlserver://localhost:1433;databaseName=OEE Point85 Point85 
+start javaw.exe -jar oee-apps-1.0.0.jar COLLECTOR jdbc:sqlserver://localhost:1433;databaseName=OEE Point85 Point85 
