93e23c63bc00a621abf996be8f4312aca5d4e4e3
Kent Randall <Point85.llc@gmail.com>
2022-07-09 08:36:45 -0700

    Added double quotes for Windows batch files path spaces

--- run-collector-app.bat
+++ run-collector-app.bat
@@ -1,2 +1,2 @@
 rem Launch the Collector application (args: JDBC connection string, user name, password and optional collector name)
-start javaw.exe -p %JAVAFX_HOME%\lib --add-modules javafx.controls,javafx.fxml,javafx.web -Dlog4j2.configurationFile=config/logging/log4j2.xml -jar oee-apps-3.8.0.jar COLLECTOR jdbc:hsqldb:hsql://localhost/OEE SA 
+start "" "%JAVA_HOME%\bin\javaw.exe" -p "%JAVAFX_HOME%\lib" --add-modules javafx.controls,javafx.fxml,javafx.web -Dlog4j2.configurationFile=config/logging/log4j2.xml -jar oee-apps-3.8.0.jar COLLECTOR jdbc:hsqldb:hsql://localhost/OEE SA 
--- run-designer-app.bat
+++ run-designer-app.bat
@@ -1,3 +1,3 @@
 rem Launch the Designer application (args: JDBC connection string, user name, password and optional collector name)
-start javaw.exe -p %JAVAFX_HOME%\lib --add-modules javafx.controls,javafx.fxml,javafx.web -Dlog4j.configurationFile=config/logging/log4j2.xml -jar oee-apps-3.8.0.jar DESIGNER jdbc:hsqldb:hsql://localhost/OEE SA 
+start "" "%JAVA_HOME%\bin\javaw.exe" -p "%JAVAFX_HOME%\lib" --add-modules javafx.controls,javafx.fxml,javafx.web -Dlog4j.configurationFile=config/logging/log4j2.xml -jar oee-apps-3.8.0.jar DESIGNER jdbc:hsqldb:hsql://localhost/OEE SA 
  
